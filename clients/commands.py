6274a5fee96d78f09eca3db27f05ed601ec079f4
Juan David Díaz <juandiazr95@gmail.com>
2022-02-19 17:18:35 -0500

    I implemente the options create and list clients

--- clients/commands.py
+++ clients/commands.py
@@ -1,4 +1,8 @@
 import click
+from clients.models import Client
+from clients.services import ClientServices
+from tabulate import tabulate
+
 
 @click.group() #convierte a la función en un decorador
 def clients():
@@ -7,17 +11,45 @@ def clients():
 
 
 @clients.command()#decimos que create es un comando
+@click.option('-n', '--name',#tenemos un nombre abreviado y el nombre completo
+                type = str, #El tipo es string
+                prompt = True, #Si no lo dan con el patrón abreviado, se lo pedimos al usuario
+                help = 'The client name') #creamos una línea de ayuda
+@click.option('-c', '--company',
+                type = str,
+                prompt = True,
+                help = 'The client company')
+@click.option('-e', '--email',
+                type = str,
+                prompt = True,
+                help = 'The client email')
+@click.option('-p', '--position',
+                type = str,
+                prompt = True,
+                help = 'The client position')
 @click.pass_context #Nos da el objeto contexto
 def create(ctx, name, company, email, position):
     """Creates a new client"""
-    pass
+    client = Client(name, company, email, position) #creamos un cliente
+    client_service = ClientServices(ctx.obj['clients_table'])
+    client_service.create_client(client)
 
 
 @clients.command()
 @click.pass_context
 def list(ctx):
     """Lists all clients"""
-    pass
+    #creamos un objeto de ClientServices y le pasamos el nombre del archivo de clientes
+    client_services = ClientServices(ctx.obj['clients_table'])
+
+    client_list = client_services.list_clients() #traemos la lista de clientes (lista de diccionarios)
+    headers = [field.upper() for field in Client.schema()] #llaves del dict para titulo de la tabla
+    table = []#será una lista que contiene en cada índice una lista con los datos del cliente
+  
+    for client in client_list:
+        table.append([client["name"], client["company"], client["email"], client["position"], client["id"]])
+
+    click.echo(tabulate(table, headers))#cumple la función de print, pero echo es para cualquier SO
 
 
 @clients.command()
--- clients/services.py
+++ clients/services.py
@@ -1,14 +1,20 @@
 import csv
+from encodings import utf_8
 from clients.models import Client
 
 
 class ClientServices:
 
     def __init__(self, table_name):
-        self.table_name = table_name #recibe nuestro el nombre de nuestro archivo .csv
+        self.table_name = table_name #recibe el nombre de nuestro archivo .csv
 
     def create_client(self, client):
-        with open(self.table_name, mode='a') as f:
+        with open(self.table_name, mode='a', encoding='utf_8') as f:
             writer = csv.DictWriter(f, fieldnames=Client.schema())#schema() retorna las llaves del dict
             writer.writerow(client.to_dict())#escribimos una nueva fila
             #el cliente que recibimos como parametro lo convertimos a dict
+
+    def list_clients(self):
+        with open(self.table_name, mode='r', encoding='utf_8') as f:
+            reader = csv.DictReader(f, fieldnames=Client.schema())
+            return list(reader) #reader es un iterable que contiene los valores del dict
